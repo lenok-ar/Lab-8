b675c005e206d0958a7670c56146ad32c7910a75
lenok-ar <leno4ka279a@gmail.com>
2024-11-20 04:08:17 +0700

    Add files via upload

---
+++ AvR lab.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    int a;
+    string r;
+    cout << "Введите арабское число (от 1 до 3999): ";
+    cin >> a;
+
+    if (a >= 1 && a <= 3999) {
+        while (a >= 1000) {
+            r += "M";
+            a -= 1000;
+        }
+        if (a >= 900) {
+            r += "CM";
+            a -= 900;
+        }
+        if (a >= 500) {
+            r += "D";
+            a -= 500;
+        }
+        if (a >= 400) {
+            r += "CD";
+            a -= 400;
+        }
+
+        while (a >= 100) {
+            r += "C";
+            a -= 100;
+        }
+        if (a >= 90) {
+            r += "XC";
+            a -= 90;
+        }
+        if (a >= 50) {
+            r += "L";
+            a -= 50;
+        }
+        if (a >= 40) {
+            r += "XL";
+            a -= 40;
+        }
+
+        while (a >= 10) {
+            r += "X";
+            a -= 10;
+        }
+        if (a >= 9) {
+            r += "IX";
+            a -= 9;
+        }
+        if (a >= 5) {
+            r += "V";
+            a -= 5;
+        }
+        if (a >= 4) {
+            r += "IV";
+            a -= 4;
+        }
+
+        while (a >= 1) {
+            r += "I";
+            a -= 1;
+        }
+        cout << "Результат конверсирования арабского числа в римское = " << r << endl;
+    }
+    return 0;
+}
---
+++ RvA lab.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    string s; 
+    cout << "Введите римское число: ";
+    cin >> s;
+
+    int n = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (i < s.length() - 1) {
+            if (s[i] == 'I' && s[i + 1] == 'V') {
+                n += 4;
+                i++;
+                continue;
+            }
+            else if (s[i] == 'I' && s[i + 1] == 'X') {
+                n += 9;
+                i++;
+                continue;
+            }
+            else if (s[i] == 'X' && s[i + 1] == 'L') {
+                n += 40;
+                i++;
+                continue;
+            }
+            else if (s[i] == 'X' && s[i + 1] == 'C') {
+                n += 90;
+                i++;
+                continue;
+            }
+            else if (s[i] == 'C' && s[i + 1] == 'D') {
+                n += 400;
+                i++;
+                continue;
+            }
+            else if (s[i] == 'C' && s[i + 1] == 'M') {
+                n += 900;
+                i++;
+                continue;
+            }
+        }
+
+        if (s[i] == 'M') {
+            n += 1000;
+        }
+        else if (s[i] == 'D') {
+            n += 500;
+        }
+        else if (s[i] == 'C') {
+            n += 100;
+        }
+        else if (s[i] == 'L') {
+            n += 50;
+        }
+        else if (s[i] == 'X') {
+            n += 10;
+        }
+        else if (s[i] == 'V') {
+            n += 5;
+        }
+        else if (s[i] == 'I') {
+            n += 1;
+        }
+    }
+    cout << "Конвертация римских чисел в арабские = " << n << endl;
+    return 0;
+}
